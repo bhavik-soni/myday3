70140050ece10e3ce565fda787159c489e6cf66f
Bhavik Soni <bhaviksoni921@gmail.com>
2023-03-17 15:26:29 -0700

    input box

--- assets/js/components/footer/Footer.js
+++ assets/js/components/footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class=" bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <footer class="flex bg-white rounded-lg shadow m-4 md:flex dark:bg-gray-800">
       <div class="w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
@@ -11,7 +11,7 @@ const Footer = () => {
           </a>
           . All Rights Reserved.
         </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <ul class="flex flex-wrap items-center justify-around mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
             <a href="#" class="mr-4 hover:underline md:mr-6 ">
               About
--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -1,11 +1,13 @@
 import NavBar from "../components/navbar/NavBar";
 import Footer from "../components/footer/Footer";
+import JournalInputBox from "../components/journalbox/JournalInputBox";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <div>
     <NavBar />
+    <JournalInputBox />
     <Footer />
   </div>
 );
