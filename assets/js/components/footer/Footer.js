bfdd77451972075838549d9bd82cbdf49e08e58c
Bhavik Soni <bhaviksoni921@gmail.com>
2023-03-18 00:19:46 -0700

    footer reactivity repair

--- assets/js/components/footer/Footer.js
+++ assets/js/components/footer/Footer.js
@@ -2,16 +2,18 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="flex bg-white rounded-lg shadow m-4 md:flex dark:bg-gray-800">
-      <div class="w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="#" class="hover:underline">
-            Goofy Aah™
-          </a>
-          . All Rights Reserved.
-        </span>
-        <ul class="flex flex-wrap items-center justify-around mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+    <footer class="m-4 flex rounded-lg bg-white shadow dark:bg-gray-800 md:flex">
+      <div class="w-full justify-self-center p-4 md:flex md:items-center md:justify-between md:p-6">
+        <div class="flex justify-center">
+          <span class="text-sm text-gray-500 dark:text-gray-400 sm:justify-self-center sm:text-center">
+            © 2023{" "}
+            <a href="#" class="hover:underline">
+              Goofy Ahh™
+            </a>
+            . All Rights Reserved.
+          </span>
+        </div>
+        <ul class="mt-3 flex flex-wrap items-center justify-around text-sm text-gray-500 dark:text-gray-400 md:mt-0">
           <li>
             <a href="#" class="mr-4 hover:underline md:mr-6 ">
               About
