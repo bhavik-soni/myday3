998c5b06784e94fbbc67232a4ff046e0844cf785
Bhavik Soni <bhaviksoni921@gmail.com>
2023-03-18 00:19:30 -0700

    fix navigation bar theme button distance

--- assets/js/components/buttons/DarkModeSwitch.js
+++ assets/js/components/buttons/DarkModeSwitch.js
@@ -14,7 +14,7 @@ const DarkModeSwitch = () => {
     <button
       type="button"
       onClick={toggleDarkMode}
-      className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+      className="relative rounded-full bg-gray-800 p-1 mr-3 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
     >
       <span className="sr-only">
         Toggle {isDarkMode ? "light" : "dark"} mode
