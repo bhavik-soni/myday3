51251664898aba101e954024fd631d772cfa458a
Ray Chen <raychen256@gmail.com>
2023-03-23 14:19:16 -0700

    update emotion data presentation

--- assets/js/components/journalbox/EmotionData.js
+++ assets/js/components/journalbox/EmotionData.js
@@ -19,20 +19,29 @@ export default function EmotionData({ data, onExit }) {
   for (let i = 0; i < Object.keys(data).length; i++) {
     const key = Object.keys(data)[i];
     emotionScoreElements.push(
-      <div key={key}>
-        {`${key}: ${roundTo(3, data[key])} ` +
-          `(${roundTo(1, emotionScores[i] * 100)}%)`}
-      </div>
+      <div key={key}>{`${key}  ${roundTo(1, emotionScores[i] * 100)}%`}</div>
     );
   }
 
   return (
     <div
       className="flex flex-col items-center
-                opacity-100 bg-gray-500 dark:bg-slate-800 dark:text-white rounded-md"
+                opacity-100 bg-gray-400 dark:bg-slate-800 dark:text-white rounded-md"
     >
       <div className="w-[100%] px-2 py-2">
-        <button onClick={onExit}>{/* change this to svg later */}X</button>
+        <button onClick={onExit}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            className="w-8 h-8"
+            viewBox="0 0 16 16"
+          >
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+          </svg>
+        </button>
+      </div>
+      <div className="px-5">
+        {`Strength: ${roundTo(1, Math.log(softmaxDenom))}`}
       </div>
       <div className="px-5 pb-5">{emotionScoreElements}</div>
     </div>
